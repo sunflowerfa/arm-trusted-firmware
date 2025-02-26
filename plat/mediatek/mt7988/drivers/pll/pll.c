92d59eee9bdee02538bd80597d4dfaaa6d8ed908
sunflowerfa <fa@kfafa.cn>
2025-02-26 20:12:12 +0800

    fix:尝试超频

--- plat/mediatek/mt7988/drivers/pll/pll.c
+++ plat/mediatek/mt7988/drivers/pll/pll.c
@@ -114,11 +114,11 @@ void mtk_pll_init(int skip_dcm_setting)
 	/* please change Vproc to 900mv via i2c */
 
 #ifdef CPU_USE_FULL_SPEED
-	INFO("ARMPLL_B_CON1 = 0x%x\n", 0xB4000000);
-	mmio_write_32(ARMPLL_B_CON1, 0xB4000000);	/* 1.8G */
+    INFO("ARMPLL_B_CON1 = 0x%x\n", 0xFA000000);  /* 2.5G */
+    mmio_write_32(ARMPLL_B_CON1, 0xFA000000);    /* 2.5G */
 #else
-	INFO("ARMPLL_B_CON1 = 0x%x\n", 0x96000000);
-	mmio_write_32(ARMPLL_B_CON1, 0x96000000);	/* 1.5G */
+    INFO("ARMPLL_B_CON1 = 0x%x\n", 0xB4000000);  /* 1.8G */
+    mmio_write_32(ARMPLL_B_CON1, 0xB4000000);    /* 1.8G */
 #endif
 	//mmio_write_32(ARMPLL_B_CON1, 0x50000000);	/* 800M */
 
